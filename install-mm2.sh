d4bf3ae76830608897a9b487ea55557137e63bb3
abcd567a <abcd567@hotmail.com>
2021-06-07 00:37:31 -0400

    Update install-mm2.sh

--- install-mm2.sh
+++ install-mm2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 VERSION=modesmixer2_rpi4_deb10_20190413
+DOWNLOAD_ID=1YY7wQNtqrPk7qtE3OB9DGkc4nDalJsCS
 INSTALL_FOLDER=/usr/share/mm2
 
 echo "Creating folder mm2"
 sudo mkdir ${INSTALL_FOLDER}
 echo "Downloading modeSMixer2 file from Google Drive"
-sudo wget -O ${INSTALL_FOLDER}/${VERSION}.tgz "https://drive.google.com/uc?export=download&id=1YY7wQNtqrPk7qtE3OB9DGkc4nDalJsCS"
+sudo wget -O ${INSTALL_FOLDER}/${VERSION}.tgz "https://drive.google.com/uc?export=download&id=${DOWNLOAD_ID}"
 
 echo "Unzipping downloaded file"
 sudo tar xvzf ${INSTALL_FOLDER}/${VERSION}.tgz -C ${INSTALL_FOLDER}
