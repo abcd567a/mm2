9d13a6ea4ed82b12de85eb8c8363e180447afc89
abcd567a <abcd567@hotmail.com>
2022-04-06 02:31:41 -0400

    Update install-mm2-64bit.sh

--- install-mm2-64bit.sh
+++ install-mm2-64bit.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 VERSION=modesmixer2_aarch64_rpi4_Raspberry_Pi_OS_20210228
-DOWNLOAD_ID=1W9tSjI8ejh2PjQsDtqqvQhWTVk1JwqVE
+#DOWNLOAD_ID=1W9tSjI8ejh2PjQsDtqqvQhWTVk1JwqVE
 INSTALL_FOLDER=/usr/share/mm2
 
 echo "Creating folder mm2"
 sudo mkdir ${INSTALL_FOLDER}
-echo "Downloading modeSMixer2 file from Google Drive"
-sudo wget -O ${INSTALL_FOLDER}/${VERSION}.tgz "https://drive.google.com/uc?export=download&id=${DOWNLOAD_ID}"    
+#echo "Downloading modeSMixer2 file from Google Drive"
+#sudo wget -O ${INSTALL_FOLDER}/${VERSION}.tgz "https://drive.google.com/uc?export=download&id=${DOWNLOAD_ID}"    
+echo "Downloading modeSMixer2 file from Github"
+sudo wget -O ${INSTALL_FOLDER}/${VERSION}.tgz "https://github.com/abcd567a/mm2/releases/download/v1/${VERSION}.tgz"    
 
 echo "Unzipping downloaded file"
 sudo tar xvzf ${INSTALL_FOLDER}/${VERSION}.tgz -C ${INSTALL_FOLDER}
